13b8ae85679abbc88c246f23d23a14d4fe90aee1
shahob-atm <shahobiddinatamurodov@gmail.com>
2024-11-30 19:02:57 +0500

    Initial commit

---
+++ src/main/java/com/example/railway_ticket_backend/RailwayTicketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.railway_ticket_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayTicketBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailwayTicketBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/railway_ticket_backend/RailwayTicketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.railway_ticket_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayTicketBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
